ce640d2f92baa211a537c2e0b0a2500ac3c86a62
djordjebradonjic <djordjebradonjic99@gmail.com>
2026-04-14 22:44:17 +0200

    fix: use environment varibale for API URL

--- frontend/lib/axios.ts
+++ frontend/lib/axios.ts
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api/v1', // Proveri da li ti je na backendu port 8080
-});
+ baseURL: `${process.env.NEXT_PUBLIC_API_URL}/api/v1`,});
 
 export default api;
