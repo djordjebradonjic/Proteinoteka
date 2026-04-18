85f99fcda48471140d27af2129fcae06fc1be23e
djordjebradonjic <djordjebradonjic99@gmail.com>
2026-04-18 22:56:11 +0200

    feat: Sitemap domain updated

--- frontend/app/robots.ts
+++ frontend/app/robots.ts
@@ -11,6 +11,6 @@ export default function robots(): MetadataRoute.Robots {
         '/private/',
       ],
     },
-    sitemap: 'https://proteinoteka.vercel.app/sitemap.xml',
+    sitemap: 'https://proteinoteka.rs/sitemap.xml',
   };
 }
